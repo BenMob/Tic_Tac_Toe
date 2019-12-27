a5e3d33549aa887403377a18ceaecab14339432b
Benjamin Ombeni <benjaminombeni3@gmail.com>
2019-12-26 23:24:08 -0800

    Wrote the draw table function

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -10,3 +10,22 @@
 #                                                     a | s | f     ->     x | o | x
 #                                                    -----------          -----------
 #                                                     z | x | c     ->     o | x | o
+
+def draw_table(table = [' ',' ',' ',' ',' ',' ',' ',' ',' ']):
+    '''
+    INPUT: (List of 9 characters)
+    TASK: Prints tic tac toa table
+    '''
+    if len(table) < 9:
+        print('Insufficient number of checks in TTT table!')
+    else:
+        print(f'\n\t{table[0]} | {table[1]} | {table[2]}')
+        print('\t---------')
+        print(f'\t{table[3]} | {table[4]} | {table[5]}')
+        print('\t---------')
+        print(f'\t{table[6]} | {table[7]} | {table[8]}\n')
+
+
+tb = ['x','x','o','x','o','o','x','o','o']
+#tb = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+help(draw_table)
